a2a8760339516ef1fa9b01ec29fd87ea48d17155
Striker299 <daiglemartin@hotmail.com>
2016-09-14 12:50:32 -0400

    Ajuster les acollade tel de demander par Nathan-San

--- Assets/_Framework/Scripts/Picking/Picking.cs
+++ Assets/_Framework/Scripts/Picking/Picking.cs
@@ -6,14 +6,12 @@ class PositionNotFound : System.Exception { }
 public class Picking : MonoBehaviour {
     public int RayMaxDistance = 5000;
 
-    public Vector3 GetWorldPosition(Vector3 mousePosition, Camera camera, int layerMask = 0xFFFFFF)
-    {
+    public Vector3 GetWorldPosition(Vector3 mousePosition, Camera camera, int layerMask = 0xFFFFFF) {
         Ray ray =  camera.ScreenPointToRay(mousePosition);
 
         RaycastHit hits;
         
-        if (Physics.Raycast(ray, out hits, RayMaxDistance, layerMask))
-        {
+        if (Physics.Raycast(ray, out hits, RayMaxDistance, layerMask)) {
             Vector3 distanceFromCamera = ray.direction * hits.distance;
 
             return distanceFromCamera + camera.transform.position;

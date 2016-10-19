ec6e5e98ba3daafc7f6fe18e5ce8911ab38edf84
DESKTOP-C13LRQ4\Striker <daiglemartin@hotmail.com>
2016-10-19 15:39:14 -0400

    Added nav mesh test with a simple scrip to show how to use it. We have to adapt it to our case

---
+++ Assets/_Framework/Scripts/Navigation/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public Transform goal;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
